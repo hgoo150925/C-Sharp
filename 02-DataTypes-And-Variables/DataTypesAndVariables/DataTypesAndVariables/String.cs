b180fefe31cf5aa3cf7c7dbf42902e74509145a9
hugoorlando <hugo.orlando2030@gmail.com>
2022-11-18 16:35:20 -0300

    string

---
+++ 02-DataTypes-And-Variables/DataTypesAndVariables/DataTypesAndVariables/String.cs
@@ -0,0 +1,16 @@
+﻿namespace DataTypesAndVariables;
+
+class String
+{
+    static void Main(string[] args)
+    {
+        string myname = "Denis";
+
+        string worldCup = "Qatar";
+
+        Console.WriteLine($"My name is {myname}");
+        Console.WriteLine($"Fifa World Cup {worldCup} 2022");
+        Console.ReadLine();
+    }
+}
+
